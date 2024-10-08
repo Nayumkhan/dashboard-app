544abfbe14c3df5eb2e50ee7714b37ee6df44833
Naeem <nk25642@gmail.com>
2024-10-08 12:35:52 +0300

    Describe your changes here

--- app.py
+++ app.py
@@ -45,6 +45,9 @@ def update_chart(*args):
     global total_trucks
     ctx = dash.callback_context
 
+    bar_fig = create_bar_chart()
+    pie_fig = create_pie_chart()
+
     if ctx.triggered:
         triggered_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
@@ -53,7 +56,7 @@ def update_chart(*args):
             if new_total is not None and new_total != '':
                 total_trucks = int(new_total)
                 df['Values'] = 0
-                return create_figures(), '', '', *([''] * len(df['Category']))
+                return bar_fig, pie_fig, '', *([''] * len(df['Category']))
 
         for i, category in enumerate(df['Category']):
             if triggered_id == f'update-button-{category.replace(" ", "_")}':
@@ -64,20 +67,31 @@ def update_chart(*args):
                         total_trucks -= value_to_add
                         df.loc[df['Category'] == category, 'Values'] += value_to_add
 
-    return create_figures(), '', '', *([''] * len(df['Category']))
+    return create_bar_chart(), create_pie_chart(), '', *([''] * len(df['Category']))
 
-def create_figures():
-    bar_fig = px.bar(df, x='Category', y='Values', title='Bar Chart of Truck Categories',
-                     color='Category',
-                     color_discrete_map={
-                         "Available Trucks": "green",
-                         "Trucks in Operation": "blue",
-                         "Waiting Load": "orange",
-                         "Under Offload": "purple",
-                         "Breakdown": "red"
-                     })
-    pie_fig = px.pie(df, values='Values', names='Category', title='Distribution of Truck Categories')
-    return bar_fig, pie_fig
+def create_bar_chart():
+    return px.bar(
+        df, 
+        x='Category', 
+        y='Values', 
+        title='Bar Chart of Truck Categories',
+        color='Category',
+        color_discrete_map={
+            "Available Trucks": "green",
+            "Trucks in Operation": "blue",
+            "Waiting Load": "orange",
+            "Under Offload": "purple",
+            "Breakdown": "red"
+        }
+    )
+
+def create_pie_chart():
+    return px.pie(
+        df, 
+        values='Values', 
+        names='Category', 
+        title='Distribution of Truck Categories'
+    )
 
 if __name__ == '__main__':
     app.run_server(debug=True)
